dcdd92eb2571620e61d2c08d67b6e794634077cc
Mark A. Hix <markahix@gmail.com>
2023-04-13 17:19:06 -0400

    nightly commit

--- Application/utilities/parametrize.py
+++ Application/utilities/parametrize.py
@@ -1,4 +1,4 @@
-from .mol2 import *
+from .future_mol2 import *
 from .frcmod import *
 from .utilities import *
 # from .OLD_frcmod import HybridizeParams
@@ -20,41 +20,46 @@ def GetMissingParams(moltype,resname,mol2,leaplog):
         f.write(leaprc_dict[moltype])
         f.write(f"{resname} = loadmol2 {mol2}\n")
         f.write(f"test = loadpdb param.pdb\n")
-        f.write("check test\n")
+        f.write("saveamberparm test test.prmtop test.inpcrd\n")
         f.write("quit\n")
 
     # Run tleap to obtain any missing parameters.
     S.call(f"tleap -f tleap.in > {leaplog}",shell=True)
-
+    print("Did we generate LeapLog?")
+    print(G(leaplog))
     # Parse the tleap logfile to get the collection of missing parameters.
     missing_params={"BONDS":[],
                     "ANGLES":[],
                     "DIHEDRALS":[],
                     "TORSIONS":[]}
-    for i,line in enumerate(open(leaplog).readlines()):
+    for line in open(leaplog).readlines():
         if "No sp2 improper torsion term for" in line:
             missing_params["TORSIONS"].append(line.split()[-1])
+            print("Found missing torsion term:",line.split()[-1])
 
-        elif "Could not find bond parameter for atom types:" in line:
+        elif "Could not find bond parameter:" in line:
             [a1,a2] = line.split(":")[-1].split("-")
             a1 = a1.strip()
             a2 = a2.strip()
             [a1,a2] = sorted([a1,a2])            
             missing_params["BONDS"].append(f"{a1:<2}-{a2:<2}")
+            print("Found missing bond term:",f"{a1:<2}-{a2:<2}")
 
-        elif "Could not find angle parameter for atom types:" in line:
+        elif "Could not find angle parameter:" in line:
             [a1,a2,a3] = line.split(":")[-1].split("-")
             a1 = a1.strip()
             a2 = a2.strip()
             a3 = a3.strip()
             [a1,a3] = sorted([a1,a3])            
             missing_params["ANGLES"].append(f"{a1:<2}-{a2:<2}-{a3:<2}")
+            print("Found missing angle term: ",f"{a1:<2}-{a2:<2}-{a3:<2}")
 
         elif "** No torsion terms for" in line:
             [a1,a2,a3,a4] = line.split(":")[-1].split("-")
             if [a3,a2] == sorted([a2,a3]):
                 [a1,a2,a3,a4] = [a4,a3,a2,a1]
             missing_params["DIHEDRALS"].append(f"{a1:<2}-{a2:<2}-{a3:<2}-{a4:<2}")
+            print("Found missing dihedral term: ",f"{a1:<2}-{a2:<2}-{a3:<2}-{a4:<2}")
     
     # Reduce the lists to unique sets of terms - no need to define the same parameter multiple times.
     for key in missing_params.keys():
@@ -67,7 +72,8 @@ def GenerateParameters(file_list,respcharges,moltype,resname):
 
     ### Run tleap with just the mol2 to figure out what parameters are missing.
     missing_params = GetMissingParams(moltype,resname,file_list["MOL2"],file_list["LeapLog"])
-    
+    print("Currently missing the following parameters:")
+    print(missing_params)
     ### Generate FRCMOD from mol2
     GenerateFRCMODFile(file_list["MOL2"],file_list["FRCMOD"],missing_params)
 
--- README.md
+++ README.md
@@ -2,4 +2,22 @@
 
 This repository contains the necessary files to build and deploy a Docker container which provides a web-based generator for forcefield parameters to be used in classical molecular dynamics.
 
-It uses a Psi4 backend for optimization and calculation of RESP charges.
+The current (public) implementation uses entirely open source engines on the back end.
+
+- Psi4 for geometry optimization
+- PsiRESP for RESP fitting
+- AmberTools for generation of `.mol2` and `.frcmod` files.
+
+The code base is designed, however, to allow users to modularly exchange certain engines.  
+For instance, the geometry optimization routine currently calls `OptimizePDB(pdbfile,charge=0,mult=1,method="scf/6-31g**")` to produce an optimized structure of the given PDB file.  Users may create their own version of this function that uses any geometry optimization package they wish, including their own, so long as the function call remains the same.
+
+#### Geometry Optimization
+As the current implementation does not have job-queuing or job-recall options yet, users may need to leave their browser open for the duration of the process.  Depending on deployment server hardware, molecule size and complexity, and method (level of theory + basis set), geometry optimizations may take a **considerable** amount of time.  As such, we strongly recommend users begin with structures that are at or near a geometric minimum, or simply elect not to check the "Optimize Structure" checkbox during job submission.
+
+Because RESP fitting and other parameters are at least somewhat dependent on geometry, the latter option is not recommended unless the submitted structure is already well-optimized.
+
+#### RESP Fitting
+The calculation of RESP charges in the current implementation uses [PsiRESP]<https://github.com/lilyminium/psiresp>, which is (currently) freely available and open-source.
+
+#### Mol2 Generation
+The construction of the `.mol2` file is performed by `antechamber` in the AmberTools suite.  However, to reduce reliance on external tools that may change in the future, work is ongoing to create a self-contained `.mol2` generator that can achieve the same results.
