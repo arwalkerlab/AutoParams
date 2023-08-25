f855a26d4d7d302fbf9a034267866d5876020f6b
markahix <42051926+markahix@users.noreply.github.com>
2023-08-25 10:33:08 -0400

    Update RunAutoParamsSite.sh

--- RunAutoParamsSite.sh
+++ RunAutoParamsSite.sh
@@ -1,7 +1,2 @@
-
-#conda activate tc-autoparams-flask
-conda activate psi4flask
-source /opt/intel/oneapi/setvars.sh
-module load TeraChem/1.9
 cd Application/
-flask run --host=0.0.0.0 --port=5005
+flask run --host=0.0.0.0 --port=5000
