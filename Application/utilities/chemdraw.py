0ef14b027cae93cb72f4853430db080d65afc70b
Mark A. Hix <markahix@gmail.com>
2023-09-22 14:14:15 -0400

    Updated PDBtoSMILES, removed openbabel dependency

--- Application/utilities/chemdraw.py
+++ Application/utilities/chemdraw.py
@@ -1,18 +1,16 @@
 import glob
 from rdkit import Chem
 from rdkit.Chem import Draw
-from openbabel import openbabel
 from .defaults import *
 from .utilities import *
 
 def PDBtoChemDraw(pdb,output):
+    mol = Chem.MolFromPDBFile(pdb)
+    smiles = Chem.MolToSmiles(mol)
     smi_file = pdb.replace(".pdb",".smi")
-    if not glob.glob(smi_file):
-        obConversion = openbabel.OBConversion()
-        obConversion.SetInAndOutFormats("pdb", "smi")
-        mol = openbabel.OBMol()
-        obConversion.ReadFile(mol, pdb)
-        obConversion.WriteFile(mol, smi_file)
-    smiles = Chem.MolToSmiles(Chem.MolFromSmiles(open(smi_file).read().split()[0]))
+    with open(smi_file,"w") as f:
+        f.write(smiles)
+        f.write("    ")
+        f.write(pdb)
     Draw.MolToFile(Chem.MolFromSmiles(smiles), output, size=(1268,720))
     return smiles
